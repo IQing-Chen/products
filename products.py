19f40dc4ca45a62a5071a458cc2fe0196de894d4
IQing-Chen <jean840112@gmail.com>
2020-10-11 11:34:29 +0800

    third ver

--- products.py
+++ products.py
@@ -6,10 +6,8 @@ while True:
 	if name == "q":
 		break
 	price = input('请输入商品价格：')
-	p = [] #大清單中的小清單
-	p.append(name)
-	p.append(price)
-	products.append(p) #小清單裝入大清單
+	 #大清單中的小清單
+	products.append([name, price]) #小清單裝入大清單
 print(products)
 
 products[0][0]
